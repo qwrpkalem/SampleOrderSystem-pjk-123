08ef0f00fe5d1341cefd17ae945e78866dc93ee4
qwrpkalem <qwrpkalem@naver.com>
2026-07-15 14:06:40 +0900

    콘솔 한글 깨짐 수정: 콘솔 코드페이지를 UTF-8로 강제 설정
    /utf-8 컴파일러 옵션으로 소스와 실행 문자셋은 UTF-8이지만, Windows
    콘솔의 기본 코드페이지(한국어 환경에서 보통 949)가 이를 잘못 해석해
    한글이 깨지는 문제 발견. main()에서 SetConsoleOutputCP(CP_UTF8),
    SetConsoleCP(CP_UTF8)를 호출해 콘솔 입출력 코드페이지를 UTF-8로
    고정하여 해결

--- SampleOrderSystem/main.cpp
+++ SampleOrderSystem/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include "service/AppContext.h"
 #include "service/ConsoleUI.h"
 
 int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
     sos::AppContext appContext("sample_order_system_data.json");
     sos::ConsoleUI ui(appContext);
     ui.run(std::cin, std::cout);
